b6d6a1708f1e1853b4443c9d761b4111b83e3766
Arnab Sen <arnabsen1729@gmail.com>
2021-04-07 18:07:28 +0530

    Normal plot

--- Q_1/Normal/normal.c
+++ Q_1/Normal/normal.c
@@ -1,50 +1,42 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
 #include <time.h>
 
 #define NMAX 10000000
 
-double mean(double *values, int n)
-{
+double mean(double *values, int n) {
     int i;
     double s = 0;
 
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
         s += values[i];
     }
     return s / n;
 }
 
-double stddev(double *values, int n)
-{
+double stddev(double *values, int n) {
     int i;
     double average = mean(values, n);
     double s = 0;
 
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
         s += (values[i] - average) * (values[i] - average);
     }
     return sqrt(s / (n - 1));
 }
 
-double *generate(int n, double miu, double sigma)
-{
+double *generate(int n, double miu, double sigma) {
     int i;
     int m = n + n % 2;
     double *values = (double *)calloc(m, sizeof(double));
     double average, deviation;
 
-    if (values)
-    {
-        for (i = 0; i < m; i += 2)
-        {
+    if (values) {
+        for (i = 0; i < m; i += 2) {
             double x, y, rsq, f;
-            do
-            {
+            do {
                 x = 2.0 * rand() / (double)RAND_MAX - 1.0;
                 y = 2.0 * rand() / (double)RAND_MAX - 1.0;
                 rsq = x * x + y * y;
@@ -57,27 +49,17 @@ double *generate(int n, double miu, double sigma)
     return values;
 }
 
-int main(void)
-{
+int main(void) {
     double *seq, miu, sigma;
-    long int n = 1000, file_num = 10;
+    long int n = 1000000, file_num = 10;
     char filename[30] = "NormalDst.txt";
     miu = 100.0;
     sigma = 10.0;
-    // printf("Mean:");
-    // scanf("%lf", &miu);
-    // printf("Std. Dev. :");
-    // scanf("%lf", &sigma);
-    //printf("Number of files you want to generate(<9):");scanf("%ld",&file_num);
-    //printf("Standardised Practical Mean = %lf || Standardised Theoretical Mean = %lf\n",mean(seq,n),miu);
-    //printf("Standardised Practical SD = %lf || Standardised Theoretical SD = %lf\n",stddev(seq,n),sigma);
-    //printf("Time Taken to Generate %ld numbers : %lf.\n",n,time);
     seq = generate(n, miu, sigma);
     FILE *fp;
     fp = fopen(filename, "w");
     fprintf(fp, "%ld\n", n);
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         fprintf(fp, "%lf\n", seq[i]);
     }
     fclose(fp);
