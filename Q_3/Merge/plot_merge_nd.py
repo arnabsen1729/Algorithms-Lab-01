21d501f03e8279f1da3a36a0d879e31a2148c9c8
Krishnendu Bera <bera.krishnendu36@gmail.com>
2021-04-07 19:06:14 +0530

    UPD: Q_3 plots

--- Q_3/Merge/plot_merge_nd.py
+++ Q_3/Merge/plot_merge_nd.py
@@ -2,22 +2,42 @@ import matplotlib.pyplot as plt
 import math
 
 
-def getArray(name):
+def getCompArray(name):
     f = open(name)
     data = f.read()
     count = [int(x.split(', ')[0]) for x in data.split('\n')[:-1]]
-    avg_time = [float(x.split(', ')[1]) for x in data.split('\n')[:-1]]
+    #avg_time = [float(x.split(', ')[1]) for x in data.split('\n')[:-1]]
     comp = [int(x.split(', ')[2]) for x in data.split('\n')[:-1]]
 
     comp_ratio = [(c/(n*math.log2(n))) for c, n in zip(comp, count)]
     return comp_ratio
 
 
-plt.title('Comparision and Time Ratio')
+def getTimeArray(name):
+    f = open(name)
+    data = f.read()
+    count = [int(x.split(', ')[0]) for x in data.split('\n')[:-1]]
+    avg_time = [float(x.split(', ')[1]) for x in data.split('\n')[:-1]]
+    #comp = [int(x.split(', ')[2]) for x in data.split('\n')[:-1]]
 
-plt.plot(getArray("Normal_comparisons.txt"), 'o-', label='Normal')
-plt.plot(getArray("Uniform_comparisons.txt"), 'o--', label='Unifrom')
-plt.legend()
-plt.ylabel(r'$\frac{comp}{n*lgn}$', rotation=0, labelpad=13)
+    comp_ratio = [(t/(n*math.log2(n))) for t, n in zip(avg_time, count)]
+    return comp_ratio
+
+
+fig, axis = plt.subplots(2, 1, figsize=(10, 7))
+
+fig.suptitle('Comparision and Time Ratio')
+
+axis[0].plot(getCompArray("Normal_comparisons.txt"), 'o-', label='Normal')
+axis[0].plot(getCompArray("Uniform_comparisons.txt"), 'o--', label='Unifrom')
+axis[0].legend()
+axis[0].set_ylabel(r'$\frac{comp}{n*lgn}$', rotation=0, labelpad=13)
+
+axis[1].plot(getTimeArray("Normal_comparisons.txt"), 'o-', label='Uniform')
+axis[1].plot(getTimeArray("Uniform_comparisons.txt"), 'o--', label='Normal')
+axis[1].legend()
+axis[1].set_ylabel(r'$\frac{time}{n*lgn}$', rotation=0, labelpad=13)
+
+plt.xlabel(r'n [array size = $2^n$]')
 
 plt.show()
